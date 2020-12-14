0416de73f0dd17b4ea2a3a027d20baf7a6daae18
Marina Sokurenko <expromtum@gmail.com>
2020-12-14 16:27:36 +0300

    Авторизация в Publisher

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -6,11 +6,16 @@ import ru.javaops.masterjava.web.WebStateException;
 
 import javax.activation.DataHandler;
 import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Service;
+import javax.xml.ws.handler.MessageContext;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MailServiceClient {
 
@@ -23,6 +28,15 @@ public class MailServiceClient {
                 new Attachment("version.html", new DataHandler(new File("config_templates/version.html").toURI().toURL())));
 
         MailService mailService = service.getPort(MailService.class);
+
+        // Авторизация
+        Map<String, Object> requestContext = ((BindingProvider)mailService).getRequestContext();
+
+        requestContext.put(BindingProvider.USERNAME_PROPERTY, "user");
+        requestContext.put(BindingProvider.PASSWORD_PROPERTY, "password");
+        requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                "http://localhost:8080/mail/mailService");
+
 
         String state = mailService.sendToGroup(ImmutableSet.of(new Addressee("masterjava@javaops.ru", null)), null,
                 "Group mail subject", "Group mail body", attachments);
